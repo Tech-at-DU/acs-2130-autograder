d54272d5331e3df4d7de3eb79e9c565e1f81eb77
Dani Roxberry <dani@bitoriented.com>
2022-02-01 13:00:14 -0800

    [add] files autograder and default gitignore for gradescope assignments

--- setup.sh
+++ setup.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
+# Install Gradescope dependencies:
 apt-get install -y python3 python3-pip python3-dev
 
-pip3 install -r /autograder/source/requirements.txt
+# Create .ssh directory and copy config files:
+mkdir -p /root/.ssh
+cp ssh_config /root/.ssh/config
+
+# Make sure to include your private key here:
+cp deploy_key /root/.ssh/deploy_key
+
+# To prevent host key verification errors at runtime:
+ssh-keyscan -t rsa github.com >>~/.ssh/known_hosts
+
+# Clone autograder repository from GitHub:
+git clone git@github.com:droxey/tweet-generator-autograder /autograder/src
+
+# Copy the latest tests to the /autograder/tests directory:
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/Tech-at-DU/ACS-1120-Intro-Data-Structures/master/Code/dictogram_test.py /autograder/src/tests/dictogram_test.py
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/Tech-at-DU/ACS-1120-Intro-Data-Structures/master/Code/listogram_test.py /autograder/src/tests/listogram_test.py
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/Tech-at-DU/ACS-1120-Intro-Data-Structures/master/Code/hashtable_test.py /autograder/src/tests/hashtable_test.py
+wget --no-check-certificate --content-disposition https://raw.githubusercontent.com/Tech-at-DU/ACS-1120-Intro-Data-Structures/master/Code/linkedlist_test.py /autograder/src/tests/linkedlist_test.py
+
+# Install the requirements from this repository:
+pip3 install -r /autograder/src/requirements.txt
--- tests/test_files.py
+++ tests/test_files.py
@@ -7,8 +7,16 @@ class TestFiles(unittest.TestCase):
     @weight(0)
     def test_submitted_files(self):
         """Check submitted files"""
-        missing_files = check_submitted_files(['calculator.py'])
-        for path in missing_files:
+        required_files = check_submitted_files([
+            'app.py',
+            'dictogram.py',
+            'hashtable.py',
+            'linkedlist.py',
+            'listogram.py',
+            'requirements.txt',
+            'runtime.txt'
+        ])
+        for path in required_files:
             print('Missing {0}'.format(path))
-        self.assertEqual(len(missing_files), 0, 'Missing some required files!')
+        self.assertEqual(len(required_files), 0, 'Missing some required files!')
         print('All required files submitted!')
